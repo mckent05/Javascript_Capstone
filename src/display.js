d5bf1205b7ae114114ce77eedfcadd829f44647f
MCKENT05 <atemitope95@gmail.com>
2021-11-16 12:45:07 +0100

    made required changes

--- src/display.js
+++ src/display.js
@@ -3,18 +3,18 @@ const fetchFilm = (id) => fetch(`https://api.tvmaze.com/lookup/shows?tvrage= ${i
 const movieCont = document.querySelector('.movie-cont');
 
 const displayMovies = (movieName, image) => {
-  movieCont.innerHTML += `<li>
+  movieCont.innerHTML += `<li class ='p-relative d-flex a-center j-center flex-dir'>
     <img src=${image} alt='' class='image'>
-    <div class='desc'>
-        <h2 class= 'item_id'>${movieName}</h2>
-        <div class='likes'>
+    <section class='desc p-relative d-flex'>
+        <h2 class= 'item_id d-flex'>${movieName}</h2>
+        <article class='likes p-relative d-flex flex-dir'>
             <i class='far fa-heart'></i>
             <p><span class='item-likes'>5</span></span><span>likes</span>
             </p>
-        </div>
-    </div>
-    <div class='btn-cont'>
-        <button class='comment-btn' data-id='comment'>comment</button>
+        </article>
+    </section>
+    <div class='btn-cont p-relative d-flex a-center j-center flex-dir'>
+        <button class='comment-btn p-relative' data-id='comment'>comment</button>
     </div>
    </li>`;
 };
