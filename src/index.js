c71ff28cefd3dad4372901e2acdbcdb8ec5bcab0
MCKENT05 <atemitope95@gmail.com>
2021-11-16 11:17:34 +0100

    fixed linter errors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './style.css';
 import getMyMovies from './display.js';
 
-const movieID = [ 35814, 22622, 28417, 38923, 27387, 28376, 24493, 2589, 41883];
+const movieID = [ 35814, 22622, 28417, 38923, 27387, 28376, 24493, 2589, 41883 ];
 
- window.addEventListener('DOMContentLoaded', () => {
-    getMyMovies(movieID)
- })
+window.addEventListener('DOMContentLoaded', () => {
+  getMyMovies(movieID);
+});
