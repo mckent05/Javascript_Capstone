510aa07b75206c6d1a731b80e76d2399d9523ad5
MCKENT05 <atemitope95@gmail.com>
2021-11-17 12:57:51 +0100

    fixed linter errors

--- src/modules/likes.js
+++ src/modules/likes.js
@@ -1,15 +1,14 @@
 const postLikes = (item) => fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/muSoj5Uz98agXzNuuOcQ/likes/',
-    {
-      method: 'POST',
-      body: JSON.stringify({
-        item_id: item,
-      }),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    }
-  );
+  {
+    method: 'POST',
+    body: JSON.stringify({
+      item_id: item,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
 
-const getLikes = () => fetch( 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/muSoj5Uz98agXzNuuOcQ/likes/');
+const getLikes = () => fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/muSoj5Uz98agXzNuuOcQ/likes/');
 
 export { postLikes, getLikes };
