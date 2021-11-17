ca91f1de4c6698d62515dbe4ab4d7185b5d30084
MCKENT05 <atemitope95@gmail.com>
2021-11-17 14:37:41 +0100

    added likes event to add new likes

--- src/modules/movies.js
+++ src/modules/movies.js
@@ -1,5 +1,6 @@
 import { getLikes } from './likes.js';
 import { modal, fetchAllPopup, fetchFilm } from './display.js';
+import  postNewLikes from './addlikes.js'; 
 
 const movieID = [
   35814, 22622, 28417, 38923, 27387, 28376, 24493, 2589, 41883, 27195,
@@ -12,7 +13,7 @@ const displayMovies = (movieName, image, likes) => {
     <section class='desc p-relative d-flex j-space-around'>
         <h2 class= 'item_id d-flex'>${movieName}</h2>
         <article class='likes p-relative d-flex flex-dir'>
-            <i class='far fa-heart'></i>
+            <i class='fas fa-heart'></i>
             <p><span class='item-likes'>${likes}</span><span>likes</span>
             </p>
         </article>
@@ -23,11 +24,25 @@ const displayMovies = (movieName, image, likes) => {
    </li>`;
 
   const allCommentBtns = document.querySelectorAll('.comment-btn');
+  const myLikes = document.querySelectorAll('.fa-heart');
+
   allCommentBtns.forEach((btn) => {
     btn.addEventListener('click', (e) => {
       const movieTitle = e.target.parentElement.previousElementSibling.children[0].innerHTML;
       modal.style.display = 'block';
       fetchAllPopup(movieID, movieTitle);
+    });
+  });
+
+  myLikes.forEach((like) => {
+    like.addEventListener('click', (e) => {
+      e.currentTarget.classList.add('red-back');
+      let item_id = e.currentTarget.parentElement.parentElement.children[0].innerHTML;
+      let newLikes = e.currentTarget.nextElementSibling.children[0].innerHTML;
+      newLikes = parseInt(newLikes);
+      postNewLikes(item_id);
+      newLikes += 1;
+      e.currentTarget.nextElementSibling.children[0].innerHTML = newLikes;
     });
   });
 };
@@ -36,7 +51,6 @@ const getMovies = async (id) => {
   try {
     const data = await (await fetchFilm(id)).json();
     const dataID = data.name;
-    // postLikes(data.name)
     let likesData = await (await getLikes()).json();
     likesData = likesData.filter((item) => item.item_id === dataID);
     displayMovies(data.name, data.image.medium, likesData[0].likes);
